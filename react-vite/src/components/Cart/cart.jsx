08f5bd1f91f483487b8d3b0e32d879192702a1c6
tyler <tylothedino@gmail.com>
2024-08-24 11:42:46 -0700

    Added a remove from cart

--- react-vite/src/components/Cart/cart.jsx
+++ react-vite/src/components/Cart/cart.jsx
@@ -7,11 +7,9 @@ const Cart = () => {
     const user = useSelector((state) => state.session.user);
 
     const [total_price, setTotalPrice] = useState(0);
+    const [amounts, setAmounts] = useState({});
+
     const cart = useLoaderData().cart.cart
-
-
-    console.log(cart)
-
 
     useEffect(() => {
         let total = 0
@@ -22,6 +20,27 @@ const Cart = () => {
     }, [cart])
 
 
+
+
+    const handleAmountChange = (productId, newAmount) => {
+        setAmounts(prevAmounts => ({
+            ...prevAmounts,
+            [productId]: newAmount
+        }));
+    };
+
+
+    const renderAmount = (max, product_id) => {
+        const options = [];
+        for (let i = 1; i <= max; i++) {
+            options.push(<option key={`${product_id}_${i}`} value={i}>{i}</option>)
+        }
+
+        return options
+    }
+
+
+
     return (
         <div>
             <div>
@@ -30,7 +49,24 @@ const Cart = () => {
                         <div key={`product_cart_${product.product.id}`}>
                             <h4>{product.amount}</h4>
                             <p>{product.product.name}</p>
+                            <img src={product.product.image} />
                             <p>{product.product.price}</p>
+
+                            <Form method="put" action={`/cart`}>
+                                <select name="amount" defaultValue={product.amount} id="product_amount" value={amounts[product.product.id]} onChange={(e) => handleAmountChange(product.product.id, +e.target.value)}>
+                                    {renderAmount(product.amount, product.product.id)}
+
+                                </select>
+
+                                <input type='hidden' value={product.product.id} name='product_id' />
+
+                                <button
+                                    type="submit"
+                                    name='intent'
+                                    value='remove-product'
+                                >Remove</button>
+                            </Form>
+
                         </div>
                     ))
                 }
