12917cc5dc124e504ee8366e701faae22adc16ce
tyler <tylothedino@gmail.com>
2024-08-29 13:39:30 -0700

    Finished sign-up css

--- react-vite/src/components/SignupFormPage/SignupFormPage.jsx
+++ react-vite/src/components/SignupFormPage/SignupFormPage.jsx
@@ -126,12 +126,12 @@ function SignupFormModal() {
     <div className="containers">
       <div className="left">
         <h2 className="titles">Welcome to the PokeMart!</h2>
-        <img className='login-image' src="https://giffiles.alphacoders.com/129/129820.gif" />
+        <img className='login-images' src="https://giffiles.alphacoders.com/129/129820.gif" />
       </div>
 
 
       <div className="right">
-        <h2 className="login-title">Sign up</h2>
+        <h2 className="logintitle">Sign up</h2>
         <div className="signup-details">
           {errors.server && <p>{errors.server}</p>}
           <form className="login-form" onSubmit={handleSubmit}>
@@ -140,6 +140,7 @@ function SignupFormModal() {
                 <div>
                   <p className="font">Email:</p>
                   <input
+                    className="bigger-input"
                     type="text"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
@@ -151,6 +152,7 @@ function SignupFormModal() {
                 <div>
                   <p className="font">Username:</p>
                   <input
+                    className="bigger-input"
                     type="text"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
@@ -169,6 +171,7 @@ function SignupFormModal() {
               <div className="split-input">
                 <p className="font">First Name:</p>
                 <input
+                  className="bigger-input"
                   type="text"
                   value={first_name}
                   onChange={(e) => setfname(e.target.value)}
@@ -182,6 +185,7 @@ function SignupFormModal() {
               <div className="split-input">
                 <p className="font">Last Name:</p>
                 <input
+                  className="bigger-input"
                   type="text"
                   value={last_name}
                   onChange={(e) => setlname(e.target.value)}
@@ -200,6 +204,7 @@ function SignupFormModal() {
               <div>
                 <p className="font">Password:</p>
                 <input
+                  className="bigger-input"
                   type="password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
@@ -213,6 +218,7 @@ function SignupFormModal() {
               <div>
                 <p className="font">Confirm Password:</p>
                 <input
+                  className="bigger-input"
                   type="password"
                   value={confirmPassword}
                   onChange={(e) => setConfirmPassword(e.target.value)}
@@ -229,6 +235,7 @@ function SignupFormModal() {
               <div>
                 <p className="font">Address:</p>
                 <input
+                  className="bigger-input"
                   type="text"
                   value={address}
                   onChange={(e) => setaddress(e.target.value)}
@@ -242,6 +249,7 @@ function SignupFormModal() {
               <div>
                 <p className="font">City:</p>
                 <input
+                  className="bigger-input"
                   type="text"
                   value={city}
                   onChange={(e) => setCity(e.target.value)}
@@ -258,6 +266,7 @@ function SignupFormModal() {
               <div>
                 <p className="font">State:</p>
                 <input
+                  className="bigger-input"
                   type="text"
                   value={state}
                   onChange={(e) => setState(e.target.value)}
@@ -271,6 +280,7 @@ function SignupFormModal() {
               <div>
                 <p className="font">Zip Code:</p>
                 <input
+                  className="bigger-input"
                   type="text"
                   value={zipcode}
                   onChange={(e) => setZip(e.target.value)}
@@ -282,7 +292,7 @@ function SignupFormModal() {
               </div>
             </div>
 
-            <button className="login-button gap-margin" type="submit">Sign Up</button>
+            <button className="login-button gap-margin font" type="submit">Sign Up</button>
           </form>
         </div>
       </div>
